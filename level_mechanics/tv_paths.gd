1c117c1372d25cedd217353d985a7bcd57c0f041
villemantyla <villemantyla1@gmail.com>
2023-05-31 16:43:23 +0300

    entrance and tv

--- level_mechanics/tv_paths.gd
+++ level_mechanics/tv_paths.gd
@@ -5,25 +5,18 @@ var tv
 var first_path_curve
 var start_rotation_of_the_first_path = deg2rad(157.816) #hard coded from blender for every path begin
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	first_path_curve = Curve3D.new()
-#	var start_p = get_parent().get_node("forest/metarig").global_transform.origin
-#	start_p.y = 0
 
-	for p in get_parent().get_node("forest/points").get_children():
+	for p in get_parent().get_node("house_entrance/path").get_children():
 		first_path_curve.add_point(p.global_transform.origin)
 	print("point count: " +str(first_path_curve.get_point_count()))
-	
+
 	set_curve(first_path_curve)
-	
+
 	tv = get_parent().get_node("tv_ver2")
-	
+
 	set_process(false)
-#
-#	print(get_curve().get_baked_points())
-#	pass # Replace with function body.
 
 
 func start_a_path():
@@ -38,5 +31,3 @@ func start_a_path():
 
 func _process(delta):
 	$PathFollow.set_offset($PathFollow.get_offset()+3.5*delta)
-	#c[0].set_offset(c[0].get_offset()+speed*delta)
-#	pass
