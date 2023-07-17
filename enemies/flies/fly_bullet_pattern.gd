8021ad724efc1db0bca0000f6c9fa7297e53c12f
villemantyla <villemantyla1@gmail.com>
2023-07-17 22:10:11 +0300

    dock

--- enemies/flies/fly_bullet_pattern.gd
+++ enemies/flies/fly_bullet_pattern.gd
@@ -1,16 +1,18 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var speed = 400
+
+var flies = []
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	flies.append($fly_bullet)
+	flies.append($fly_bullet2)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	for fly in flies:
+		var pf = fly.get_node("PathFollow2D")
+		pf.set_offset(pf.get_offset()+speed*delta)
