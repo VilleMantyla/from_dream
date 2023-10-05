10eae00fb6334834f724d75d0aded394935186c5
villemantyla <villemantyla1@gmail.com>
2023-10-05 18:43:06 +0300

    update

--- enemies/flies/fly_bullet_pattern.gd
+++ enemies/flies/fly_bullet_pattern.gd
@@ -1,15 +1,15 @@
 extends Node2D
 
 var speed = 400
-var delay_between_flies = 1.0
-const run_time = 5.5 #seconds till this pattern is finished
+var delay_between_flies = 1.7
+const run_time = 10.5 #seconds till this pattern is finished
 
 var flies = []
 var run_time_count = run_time
 
 func _ready():
-	flies.append($fly_bullet)
-	flies.append($fly_bullet2)
+	#flies.append($fly_bullet)
+	#flies.append($fly_bullet2)
 	disable_colliders(true)
 	set_process(false)
 
@@ -18,6 +18,13 @@ func disable_colliders(val):
 		fly.get_node("PathFollow2D/Area2D/CollisionShape2D").disabled = val
 
 func reset_pattern():
+	flies = []
+	randomize()
+	var temp = [$fly_bullet, $fly_bullet2, $fly_bullet3, $fly_bullet4, $fly_bullet5]
+	temp.shuffle()
+	flies.append(temp.pop_front())
+	flies.append(temp.pop_front())
+	flies.append(temp.pop_front())
 	for i in flies.size():
 		var pf = flies[i].get_node("PathFollow2D")
 		pf.set_offset(0)
